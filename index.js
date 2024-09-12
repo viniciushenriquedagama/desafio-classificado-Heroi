d74ea80b8346293fa13cda1fa6ca17dbbd375eb5
Vinicius-Henrique <viniciushenriquegamamoreira@gmail.com>
2024-09-12 20:10:51 -0300

    nova uptade

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const NameHero = 'Vinicius';
-const NivelHero = 10000;
+let nomeHeroi = ['batman', 'Capitão América', 'Homem de Ferro', 'Superman', 'Sentinela', 'Jesus'];
+let xpHeroi = [3049, 5499, 7054, 8777, 9900, 77777]
 
-if(NivelHero <= 1000){
-    console.log('O Herói de nome', NameHero, 'está no nível de Ferro')
-}else if(NivelHero >=1001 && NivelHero <=2000){
-    console.log('O Herói de nome', NameHero, 'está no nivel de Bronze')
-}else if(NivelHero >=2001 && NivelHero <=5000){
-    console.log('O Herói de nome', NameHero, 'está no nivel Prata')
-}else if(NivelHero >=5001 && NivelHero <=7000){
-    console.log('O Herói de nome', NameHero, 'está no nivel Ouro')
-}else if(NivelHero >=7001 && NivelHero<=8000){
-    console.log('O Herói de nome', NameHero, 'está no nivel Platina')
-}else if(NivelHero >=8001 && NivelHero<=9000){
-    console.log('O Herói de nome', NameHero, 'está no nivel Ascendente')
-}else if(NivelHero >=9001 && NivelHero<=10000){
-    console.log('O Herói de nome', NameHero, 'está no nivel Imortal')
-}else if(NivelHero > 10001){
-    console.log('O Herói de nome', NameHero, 'está no nivel Radiante')
+for (let i = 0; i < nomeHeroi.length; i++) {
+    let nome = nomeHeroi[i]
+    let xp = xpHeroi[i]
+    let nivel
+
+    if (xp < 1000) {
+        nivel = 'Ferro'
+    } else if (xp >= 1001 && xp <= 2000) {
+        nivel = 'Bronze'
+    } else if (xp >= 2001 && xp <= 5000) {
+        nivel = 'Prata'
+    } else if (xp >= 5001 && xp <= 7000) {
+        nivel = 'Ouro'
+    } else if (xp >= 7001 && xp <= 8000) {
+        nivel = 'Plantina'
+    } else if (xp >= 8001 && xp <= 9000) {
+        nivel = 'Ascendente'
+    } else if (xp >= 9001 && xp <= 10000) {
+        nivel = 'Imortal'
+    } else if (xp >= 10001) {
+        nivel = 'Radiante'
+    }
+    console.log(`O Heroi de nome ${nome} está no nivel ${nivel}`)
 }
